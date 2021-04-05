87c1638284b1d700aefc732269881f4a5079622a
rajeshmanchikanti10 <rajeshmanchikanti10@gmail.com>
2021-04-05 21:14:07 +0530

    "Added admin login functonality"

---
+++ app/src/main/java/com/example/ratecommerce/AdminconfigureActivity.java
@@ -0,0 +1,14 @@
+package com.example.ratecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminconfigureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminconfigure);
+    }
+}
